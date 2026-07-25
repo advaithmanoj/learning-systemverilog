94a18b60bc8ff8c3499255145380cc88b24ae1d9
Advaith Manoj <88617390+advaithmanoj@users.noreply.github.com>
2026-07-25 18:22:25 +0530

    Update deepcopy.sv

--- classes/deepcopy.sv
+++ classes/deepcopy.sv
@@ -1,1 +1,2 @@
-
+//if u copy using deepcopy from one object to another object,not only variables,methods of the prev object gets copied to the new one.
+//This copies all the class members of its nested class members to the new one
